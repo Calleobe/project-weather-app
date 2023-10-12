92397e01520f9fbfadfae774261a98bc3a01c7b5
Fabio <cassisafabio97@gmail.com>
2023-10-12 22:42:44 +0200

    merged manually

--- script.js
+++ script.js
@@ -14,45 +14,47 @@ function displayWeatherData(data) {
   let weatherCondition = data.weather[0].main;
 
   let categorizedWeather;
-  if (weatherCondition === "Clear") {
-    categorizedWeather = "Sunny";
-    weatherIcon.innerHTML = `<img src="design/design2/icons/noun_Sunglasses_2055147.svg" alt="Weather Icon">`;
-    cityName.textContent = `Get your sunnies on. \n${data.name} is looking rather great today.`;
-  } else if (weatherCondition === "Clouds") {
-    categorizedWeather = "Cloudy";
-    weatherIcon.innerHTML = `<img src="design/design2/icons/noun_Cloud_1188486.svg" alt="Weather Icon">`;
-    cityName.textContent = `Light a fire and get cosy. \n${data.name} is looking grey today.`;
-  } else if (["Rain", "Drizzle"].includes(weatherCondition)) {
-    categorizedWeather = "Rainy";
-    weatherIcon.innerHTML = `<img src="design/design2/icons/noun_Umbrella_2030530.svg" alt="Weather Icon">`;
-    cityName.textContent = `Don't forget your umbrella. \nIt's wet in ${data.name} today.`;
-  } else {
-    categorizedWeather = "Other";
-    cityName.textContent = ` ${data.name} indicates ${weatherCondition} today.`;
-  }
-  document.body.setAttribute("data-weather", categorizedWeather);
+  let weatherIconPath;
 
-  function formatTime(date) {
-    const options = {
-      hour: "2-digit",
-      minute: "2-digit",
-      timeZone: "CET",
-    };
-    const formattedTime = new Intl.DateTimeFormat("default", options).format(
-      date
-    );
-    return formattedTime.replace(":", ".");
+  switch (weatherCondition) {
+    case "Clear":
+      categorizedWeather = "Sunny";
+      weatherIconPath = "design/design2/icons/noun_Sunglasses_2055147.svg";
+      cityName.textContent = `Get your sunnies on. ${data.name} is looking rather great today.`;
+      break;
+    case "Clouds":
+      categorizedWeather = "Cloudy";
+      weatherIconPath = "design/design2/icons/noun_Cloud_1188486.svg";
+      cityName.textContent = `Light a fire and get cosy. ${data.name} is looking grey today.`;
+      break;
+    case "Rain":
+    case "Drizzle":
+      categorizedWeather = "Rainy";
+      weatherIconPath = "design/design2/icons/noun_Umbrella_2030530.svg";
+      cityName.textContent = `Don't forget your umbrella. It's wet in ${data.name} today.`;
+      break;
+    case "Snow":
+      categorizedWeather = "Snowy";
+      weatherIconPath = "design/design2/icons/Snow.svg";
+      cityName.textContent = `Get warm clothes for these flakes outside.`;
+      break;
+    default:
+      categorizedWeather = "Other";
+      cityName.textContent = `${data.name} indicates ${weatherCondition} today.`;
   }
+
+  document.body.setAttribute("data-weather", categorizedWeather);
+  searchButton.setAttribute("data-weather", categorizedWeather);
+  cityInput.setAttribute("data-weather", categorizedWeather);
+
   const sunriseTime = new Date(data.sys.sunrise * 1000);
   const sunsetTime = new Date(data.sys.sunset * 1000);
-  const formattedSunrise = formatTime(sunriseTime);
-  const formattedSunset = formatTime(sunsetTime);
+  citySunrise.textContent = `sunrise ${formatTime(sunriseTime)}`;
+  citySunset.textContent = `sunset ${formatTime(sunsetTime)}`;
+  temperature.textContent = `${data.weather[0].description} | ${(data.main.temp - 273.15).toFixed(1)}°C`;
+  weatherIcon.innerHTML = `<img src="${weatherIconPath}" alt="Weather Icon">`;
 
-  citySunrise.textContent = `sunrise ${formattedSunrise}`;
-  citySunset.textContent = `sunset ${formattedSunset}`;
-  temperature.textContent = `${data.weather[0].description} | ${(
-    data.main.temp - 273.15
-  ).toFixed(1)}°C`;
+  fetchWeeklyForecast(data.name);
 }
 
 function fetchWeatherByCoordinates(lat, lon) {
@@ -66,32 +68,17 @@ function fetchWeatherByCoordinates(lat, lon) {
     });
 }
 
-function fetchWeeklyForecast(city = null, lat = null, lon = null) {
-  let forecastUrl = city
-    ? `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
-    : `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-  fetch(forecastUrl)
+function fetchWeeklyForecast(city) {
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
     .then((response) => response.json())
     .then((forecastData) => {
-      const dailyForecasts = forecastData.list.filter((item) =>
-        item.dt_txt.includes("12:00:00")
-      );
+      const dailyForecasts = forecastData.list.filter((item) => item.dt_txt.includes("12:00:00"));
       weeklyForecast.innerHTML = "";
       dailyForecasts.forEach((forecast) => {
         const date = new Date(forecast.dt * 1000);
-        const day = date.toLocaleDateString("en-US", {
-          weekday: "short",
-        });
-        const icon = forecast.weather[0].icon;
+        const day = date.toLocaleDateString("en-US", { weekday: "short" });
         const temp = (forecast.main.temp - 273.15).toFixed(1);
-        const description = forecast.weather[0].description;
-        const forecastItem = `
-                    <div class="forecast-item">
-                        <p>${day}</p>
-                        <p>${temp}°</p>
-                    </div>
-                `;
+        const forecastItem = `<div class="forecast-item"><p>${day}</p><p>${temp}°</p></div>`;
         weeklyForecast.innerHTML += forecastItem;
       });
     })
@@ -104,7 +91,6 @@ function showPosition(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   fetchWeatherByCoordinates(lat, lon);
-  fetchWeeklyForecast(null, lat, lon); //Fetch the weekly forecast using the coordinats.
 }
 
 if (navigator.geolocation) {
@@ -116,7 +102,6 @@ if (navigator.geolocation) {
       .then((data) => {
         displayWeatherData(data);
       });
-    fetchWeeklyForecast(defaultCity);
   });
 } else {
   const defaultCity = "Göteborg";
@@ -125,20 +110,22 @@ if (navigator.geolocation) {
     .then((data) => {
       displayWeatherData(data);
     });
-  fetchWeeklyForecast(defaultCity);
 }
 
 searchButton.addEventListener("click", () => {
   const city = cityInput.value;
   if (city) {
-    fetch(`${apiUrl}?q=${city}&appid=${apiKey}`)
-      .then((response) => response.json())
-      .then((data) => {
-        displayWeatherData(data);
-        fetchWeeklyForecast(city); // Fetch the weekly forecast using city.
-      })
-      .catch((error) => {
-        console.error("Error fetching current weather data:", error);
-      });
+    fetchWeatherData(city);
   }
 });
+
+// Function to format time
+function formatTime(date) {
+  const options = {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "CET",
+  };
+  const formattedTime = new Intl.DateTimeFormat("default", options).format(date);
+  return formattedTime.replace(":", ".");
+}
