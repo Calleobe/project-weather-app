d3c56303f306629f1293c95a85a3531629ea361e
Fabio <cassisafabio97@gmail.com>
2023-10-12 22:35:16 +0200

    refactored and added geo to my branch

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const apiKey = "8b5546cda6f5f19b75c0a17daab06b19";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+
 const cityInput = document.getElementById("cityInput");
 const searchButton = document.getElementById("searchButton");
-const searchForm = document.getElementById("searchForm");
 const cityName = document.getElementById("cityName");
 const weatherIcon = document.getElementById("weatherIcon");
 const temperature = document.getElementById("temperature");
@@ -10,35 +10,9 @@ const citySunrise = document.getElementById("sunrise");
 const citySunset = document.getElementById("sunset");
 const weeklyForecast = document.getElementById("weeklyForecast");
 
-// Function to fetch weather data and update the UI
-function fetchWeatherData(city) {
-  fetch(`${apiUrl}?q=${city}&appid=${apiKey}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const weatherCondition = data.weather[0].main;
-      updateUI(weatherCondition, data);
-      clearError(); // Clear the error message
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-      displayError("City not found, maybe a typo? Try again."); // Display an error message
-    });
-}
+function displayWeatherData(data) {
+  let weatherCondition = data.weather[0].main;
 
-// Function to display an error message, if the city is not present:
-function displayError(message) {
-  const errorElement = document.getElementById("error");
-  errorElement.textContent = message;
-}
-
-// Function to clear the error message
-function clearError() {
-  const errorElement = document.getElementById("error");
-  errorElement.textContent = "";
-}
-
-// Function to update the UI based on weather condition
-function updateUI(weatherCondition, data) {
   let categorizedWeather;
   let weatherIconPath;
 
@@ -80,11 +54,20 @@ function updateUI(weatherCondition, data) {
   temperature.textContent = `${data.weather[0].description} | ${(data.main.temp - 273.15).toFixed(1)}°C`;
   weatherIcon.innerHTML = `<img src="${weatherIconPath}" alt="Weather Icon">`;
 
-  // Fetch and display the weekly forecast
   fetchWeeklyForecast(data.name);
 }
 
-// Function to fetch and display the weekly forecast
+function fetchWeatherByCoordinates(lat, lon) {
+  fetch(`${apiUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      displayWeatherData(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data based on coordinates:", error);
+    });
+}
+
 function fetchWeeklyForecast(city) {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
     .then((response) => response.json())
@@ -104,6 +87,38 @@ function fetchWeeklyForecast(city) {
     });
 }
 
+function showPosition(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  fetchWeatherByCoordinates(lat, lon);
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition, () => {
+    // Default city if geolocation fails...
+    const defaultCity = "Göteborg";
+    fetch(`${apiUrl}?q=${defaultCity}&appid=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        displayWeatherData(data);
+      });
+  });
+} else {
+  const defaultCity = "Göteborg";
+  fetch(`${apiUrl}?q=${defaultCity}&appid=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      displayWeatherData(data);
+    });
+}
+
+searchButton.addEventListener("click", () => {
+  const city = cityInput.value;
+  if (city) {
+    fetchWeatherData(city);
+  }
+});
+
 // Function to format time
 function formatTime(date) {
   const options = {
@@ -114,23 +129,3 @@ function formatTime(date) {
   const formattedTime = new Intl.DateTimeFormat("default", options).format(date);
   return formattedTime.replace(":", ".");
 }
-
-// Event listener for the form submission
-searchForm.addEventListener("submit", (event) => {
-  event.preventDefault(); // Prevent the default form submission
-  const city = cityInput.value;
-  if (city) {
-    fetchWeatherData(city);
-  }
-});
-
-// Event listener for the search button
-searchButton.addEventListener("click", () => {
-  const city = cityInput.value;
-  if (city) {
-    fetchWeatherData(city);
-  }
-});
-
-//Start with Stockholm data: 
-fetchWeatherData("Stockholm");
